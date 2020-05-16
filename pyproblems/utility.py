971578157c19e0fa8cd2acdb6b6f5571043a5c33
Lakshmi Narayanan N <grabyourfreedom@gmail.com>
2020-05-16 17:34:24 +0530

    commit

--- pyproblems/utility.py
+++ pyproblems/utility.py
@@ -1,4 +1,7 @@
+'''This module contains all utility functions'''
 def is_int(number):
+    '''This function returns True is the number is an integer,
+    otherwise false'''
     if isinstance(number, int) and (not isinstance(number, bool)):
         return True
     return False
