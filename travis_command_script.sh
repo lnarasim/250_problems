136a8b3f41d20ff8f7522c66e989a585081dfa10
Lakshmi Narayanan N <grabyourfreedom@gmail.com>
2020-05-16 17:04:32 +0530

    commit

--- travis_command_script.sh
+++ travis_command_script.sh
@@ -1,11 +1,11 @@
 echo 'Running pylint and finding potential bad practices/bugs'
 python -m pylint source
 pylint_status = $?
-echo 'pylint status', $?
+echo 'pylint status', $pylint_status
 
 echo 'Running pytest on the entire project'
 python -m pytest -v tests/
 pytest_status = $?
-echo 'pytest status', $?
+echo 'pytest status', $pytest_status
 
 exit $pylint_status | $pytest_status
