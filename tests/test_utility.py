d35dedc9296a3fc2b1b798f6ad8b67508e8f6629
Lakshmi Narayanan N <grabyourfreedom@gmail.com>
2020-05-16 16:23:43 +0530

    utility

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -5,3 +5,6 @@ def test_is_int():
     assert is_int(10.1) == False
     assert is_int("10") == False
     assert is_int([1, 2, 3]) == False
+    assert is_int((1, 2, 3)) == False
+    assert is_int({1: 2, 2: 2}) == False
+    assert is_int(True) == False
