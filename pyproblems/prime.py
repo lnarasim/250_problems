5a888a480716cb845ede42740639063f7e007de3
Lakshmi Narayanan N <grabyourfreedom@gmail.com>
2020-05-16 17:32:53 +0530

    commit

--- pyproblems/prime.py
+++ pyproblems/prime.py
@@ -1,6 +1,9 @@
+'''This module contains prime number related function'''
 from pyproblems.utility import is_int
 
 def is_prime(number):
+    '''This fucntion returns True if the number is prime, 
+    False otherwise'''
     if not is_int(number):
         raise TypeError(f"Unsupported Type {type(number)}")
 
