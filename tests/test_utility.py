55cf4bf19e7c9fbf5750e52dc00b18132fd6b032
Lakshmi Narayanan N <grabyourfreedom@gmail.com>
2020-05-17 17:40:23 +0530

    Added is_str function and tests

--- tests/test_utility.py
+++ tests/test_utility.py
@@ -1,4 +1,5 @@
 from pyproblems.utility import is_int
+from pyproblems.utility import is_str
 
 def test_is_int():
     assert is_int(10) == True
@@ -13,3 +14,16 @@ def test_is_int():
     assert is_int(1000) == True
     assert is_int(10000000) == True
     
+def test_is_str():
+    assert is_str("123")
+    assert is_str("hello")
+    assert is_str("""123""")
+    assert is_str("10")
+    assert is_str(10.1) == False    
+    assert is_str([1, 2, 3]) == False
+    assert is_str((1, 2, 3)) == False
+    assert is_str({1: 2, 2: 2}) == False
+    assert is_str(True) == False
+    assert is_str(None) == False
+    assert is_str(set()) == False
+    assert is_str(1000) == False
