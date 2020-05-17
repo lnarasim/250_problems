5720600bd941f29b05279276ba14677e15234fb4
Lakshmi Narayanan N <grabyourfreedom@gmail.com>
2020-05-17 17:50:56 +0530

    Palindrome, code and UT

---
+++ pyproblems/palindrome.py
@@ -0,0 +1,11 @@
+"""This module contains functions that works on palindrom"""
+from pyproblems.utility import is_str
+
+def is_palindrome(s_param):
+    '''Returns True if passed string is palindrome, False if 
+    the string is not a palindrome'''
+
+    if not is_str(s_param):
+        raise TypeError(f"Unsupported Type f{type(s_param)}")
+
+    return s_param == s_param[::-1]
---
+++ tests/test_palindrome.py
@@ -0,0 +1,19 @@
+from pyproblems.palindrome import is_palindrome
+import pytest
+
+def test_palindrome_valid():
+    assert is_palindrome('111')
+    assert is_palindrome('malayalam')
+    assert is_palindrome('liril')
+
+def test_palindrome_invalid():
+    assert not is_palindrome("something")
+    assert not is_palindrome("everything")
+    assert not is_palindrome("nothing")
+
+def test_palindrome_invalid_types():
+    invalid = [("123", "456", "789"), (6, "1", "3"), {1: "123"}, None, {123, 123, 123}, True, False]
+    
+    for value in invalid:
+        with pytest.raises(TypeError):
+            is_palindrome(value)
