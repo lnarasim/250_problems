712540d3bd260292d2eb715e6c3876d72e8aef23
JothiMadhu <m.jmsudhan@gmail.com>
2020-06-02 00:46:10 +0530

    Program to sort complex number on its real part.

---
+++ pyproblems/complex_number_sorter.py
@@ -0,0 +1,12 @@
+'''This program will sort the passed sequence of complex number'''
+from pyproblems.utility import is_int, is_float
+
+def sort_complex_numbers(*complex_numbers):
+    '''The function returns a sorted tuple of passed complex numbers
+    based on the real part of those complex numbers'''
+    for i in complex_numbers:
+        if not is_int(i) and not is_float(i) and not isinstance(i, complex):
+            raise TypeError("Unsupported format")
+    list_complex = list(complex_numbers)
+    list_complex.sort(key=lambda real_part: real_part.real)
+    return tuple(list_complex)
