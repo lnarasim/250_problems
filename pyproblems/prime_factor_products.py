37c65634c5a536b2526220bc01f7e81b3b4dea54
dhanavendan <dhanavendan001@gmail.com>
2020-06-01 09:49:23 +0530

    Renamed 'num1' to number

--- pyproblems/prime_factor_products.py
+++ pyproblems/prime_factor_products.py
@@ -2,16 +2,16 @@
 from pyproblems.utility import is_int
 from pyproblems.prime import is_prime
 
-def product_prime(num1):
+def product_prime(number):
     '''This function returns the product of the numbers which are prime factors
     of a given number'''
-    if not is_int(num1):
-        raise TypeError(f'unsupported format, {type(num1)} is in the place of"integer"')
-    if num1 <= 0:
+    if not is_int(number):
+        raise TypeError(f'unsupported format, {type(number)} is in the place of"integer"')
+    if number <= 0:
         raise ValueError("pass an integer greater than 0")
     product = 1
-    for i in range(2, num1+1):
-        if num1 % i == 0:
+    for i in range(2, number+1):
+        if number % i == 0:
             if is_prime(i):
                 product = product * i
     return product
